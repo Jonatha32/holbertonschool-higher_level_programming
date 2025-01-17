0d84cc65e4005885a2452f173f5a13e9e265766e
Jonatha32 <jonaperez038@gmail.com>
2025-01-17 16:57:09 +0000

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for a in str:
+        if ord(a) >= 97 and ord(a) <= 122:
+            mayus = chr(ord(a) - 32)
+        else:
+            mayus = a
+        print(mayus, end="")
+    print()
