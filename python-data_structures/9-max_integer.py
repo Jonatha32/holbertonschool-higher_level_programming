385b9007ecc3353674bc1df4acd023a7574e3dc5
Jonatha32 <jonaperez038@gmail.com>
2025-01-23 13:37:07 +0000

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    a = len(my_list)
-    if my_list == "":
+    
+    if not my_list:
         return (None)
+
     mayor = my_list[0]
 
     for i in my_list:
