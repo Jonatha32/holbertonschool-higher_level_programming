903c66d0f6088257ef11f899081cc62292b309a0
Jonatha32 <jonaperez038@gmail.com>
2025-02-01 11:52:19 -0300

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module defines an empty class Rectangle."""
+
+
+class Rectangle:
+    """This class defines a square"""
+    def __init__(self, width=0, height=0):
+        """Initilizes a new rectangle.
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
