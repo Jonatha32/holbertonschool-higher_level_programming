3f4a7ebe3c8cc76b1bfc4e40ae882d5aaa7dbdd8
Jonatha32 <jonaperez038@gmail.com>
2025-02-03 16:34:31 -0300

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class MyList that inherits from list.
+"""
+
+
+class MyList(list):
+    """
+    A subclass of list that provides an adittional method to print
+    the elements of the list.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the elements
+        """
+        self.sort()
+        print(self)
