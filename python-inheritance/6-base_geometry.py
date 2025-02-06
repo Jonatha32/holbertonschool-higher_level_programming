4cea2cf2bc8fd636c46051951fccbb42df27c37f
Jonatha32 <jonaperez038@gmail.com>
2025-02-06 16:25:16 -0300

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Create a new empty class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    New Class
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
