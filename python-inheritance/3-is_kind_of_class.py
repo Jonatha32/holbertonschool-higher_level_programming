d9f9181b00b6a25081914be29aec7639650001ca
Jonatha32 <jonaperez038@gmail.com>
2025-02-06 12:18:48 -0300

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module returns True if the object is exactly an instance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if the object is an instance
+    """
+
+    return (isinstance(obj, a_class))
