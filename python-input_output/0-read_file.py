12ab316d5f751e5e87e0a3ba7e9433adc87669c5
Jonatha32 <jonaperez038@gmail.com>
2025-02-10 18:42:42 -0300

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Module to read and print the content of a file
+"""
 
 
 def read_file(filename=""):
-    with open(filename, "r") as f:
+    """
+    Reads a text file (UTF-8) and prints its content to stdout
+    """
+    with open(filename, "r", encoding="utf-8") as f:
         contenido = f.read()
-        print(contenido)
+        print(contenido, end="")
