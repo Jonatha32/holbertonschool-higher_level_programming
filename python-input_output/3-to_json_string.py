b1cbd49f3d374f3bf27fa6c7a10ef15ec70de676
Jonatha32 <jonaperez038@gmail.com>
2025-02-14 16:29:49 -0300

    task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+
+"""
+Module Json Voorhees
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Write a function
+    """
+    x = json.dumps(my_obj)
+
+    return (x)
