f6f0d0d358c724ce3c0711d908cbadf93467866c
Jonatha32 <jonaperez038@gmail.com>
2025-02-14 16:44:40 -0300

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module Json Voorhees
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Write a function
+    """
+    x = json.loads(my_str)
+
+    return (x)
