f488aba0a53c5b9272de2e3f209fac580d20466c
Jonatha32 <jonaperez038@gmail.com>
2025-02-15 16:50:59 -0300

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,13 +3,17 @@ import sys
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
+# name of the file
 filename = "add_item.json"
 
+# try to charge the file list
 try:
     items = load_from_json_file(filename)
 except FileNotFoundError:
     items = []
 
+# add the arguments to the list
 items.extend(sys.arg[1:])
 
+# save the updated list in the file
 save_to_json_file(items, filename)
