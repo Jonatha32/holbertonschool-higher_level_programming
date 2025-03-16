3a80f5636925e01123ddd1c09d7dd380dc866428
Jonatha32 <jonaperez038@gmail.com>
2025-03-16 16:46:15 -0300

    tassk 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N from the database hbtn_0e_0_usa"""
+"""Lists all states with a name starting with N from hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     datab = MySQLdb.connect(host='localhost', user=username,
                             passwd=password, db=database, port=3306)
     curso = datab.cursor()
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     rows = curso.fetchall()
     for row in rows:
         print(row)
-        
+
     curso.close()
     datab.close()
