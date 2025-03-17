d948fc58a033d394413818264145798c052da593
Jonatha32 <jonaperez038@gmail.com>
2025-03-17 10:14:35 -0300

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Displays all values in the states table where name matches the argument"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    
+    datab = MySQLdb.connect(host='localhost', user=username,
+                            passwd=password, db=database, port=3306)
+    curso = datab.cursor()
+    query = ("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+            .format(state_name)
+    )
+    curso.execute(query)
+    
+    rows = curso.fetchall()
+    for row in rows:
+        print(row)
+    curso.close()
+    datab.close()
