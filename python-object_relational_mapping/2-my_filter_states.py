80d9c76feb247088dd78c94efcdb7b77759c46b3
Jonatha32 <jonaperez038@gmail.com>
2025-03-17 10:21:14 -0300

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                             passwd=password, db=database, port=3306)
     curso = datab.cursor()
     query = (
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
         .format(state_name)
     )
     curso.execute(query)
