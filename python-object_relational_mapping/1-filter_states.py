f3c4c0f945a75920ef57e10df22df2cfc1130e1f
Jonatha32 <jonaperez038@gmail.com>
2025-03-17 09:34:40 -0300

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
     datab = MySQLdb.connect(host='localhost', user=username,
                             passwd=password, db=database, port=3306)
     curso = datab.cursor()
-    curso.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    curso.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' "
+        "ORDER BY id ASC"
+    )
     rows = curso.fetchall()
     for row in rows:
         print(row)
