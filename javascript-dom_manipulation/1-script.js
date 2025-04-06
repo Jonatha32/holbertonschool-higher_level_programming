1fef118bd774925731f1814f264010b4f4b83196
Jonatha32 <jonaperez038@gmail.com>
2025-04-06 15:37:12 -0300

    task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  const redHeaderElement = document.getElementById('red_header');
+    redHeaderElement.addEventListener('click', function() {
+      const headerElement = document.querySelector('header');
+      headerElement.style.color = '#FF0000';
+    });
+});
