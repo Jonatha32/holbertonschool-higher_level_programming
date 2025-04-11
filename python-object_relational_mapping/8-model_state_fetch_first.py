ab7f3abb5873bb9f1417ead90f1d056baefad858
Jonatha32 <jonaperez038@gmail.com>
2025-04-11 16:32:52 -0300

    task 8 9

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,26 +12,26 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
-        
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             username, password, database
         ),
         pool_pre_ping=True
     )
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     first_state = session.query(State).order_by(State.id).first()
-    
+
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
-    
+
     session.close()
--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,21 +12,21 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             username, password, database
         ),
         pool_pre_ping=True
     )
-    
+
     Se = sessionmaker(bind=engine)
     session = Se()
-    
+
     states = session.query(State)\
                     .filter(State.name.like('%a%'))\
                     .order_by(State.id)\
